a6ef97517645c393f8e0442d73792c6e63ab1c3e
Fabio Ricali <ricalifabio@gmail.com>
2017-11-19 11:20:02 +0100

    add UMD support

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
     output: {
         filename: './dist/incache.min.js',
         library: 'incache',
-        umdNamedDefine: true
+        umdNamedDefine: true,
+        libraryTarget: 'umd'
     },
     resolve: {
         modules: ['node_modules'],
