76f8de5b844a719672f9fc386fb99e84d45e8916
Fabio Ricali <ricalifabio@gmail.com>
2017-12-04 23:14:59 +0100

    v6.6.1

--- dist/incache.js
+++ dist/incache.js
@@ -1,4 +1,4 @@
-// [AIV]  InCache Build version: 6.6.0  
+// [AIV]  InCache Build version: 6.6.1  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
